6aaa0bc0ace6cb5ef174b21730cbf031dbc23019
Cazle <kykyglt@gmail.com>
2023-07-03 18:05:02 +0200

    First commit and making folders for each file

---
+++ CountOnMe/Model/Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  Calculator.swift
+//  CountOnMe
+//
+//  Created by Kyllian GUILLOT on 03/07/2023.
+//  Copyright © 2023 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+class Calculator {
+    
+}
---
+++ CountOnMeTests/CalculatorTests.swift
@@ -0,0 +1,18 @@
+//
+//  CountOnMeTests.swift
+//  CountOnMeTests
+//
+//  Created by Kyllian GUILLOT on 03/07/2023.
+//  Copyright © 2023 Vincent Saluzzo. All rights reserved.
+//
+
+import XCTest
+@testable import CountOnMe
+
+final class CountOnMeTests: XCTestCase {
+
+    func testCalculatorIsHere(){
+        let calc = Calculator()
+    }
+
+}
