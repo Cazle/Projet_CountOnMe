42ce0318e4317ffd02127d001d77a4552c272817
Cazle <kykyglt@gmail.com>
2023-07-20 14:27:22 +0200

    Finished priority

--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -28,14 +28,14 @@ class Calculator {
         return text.firstIndex(of: "=") != nil
     }
     
-    func resetingCalculator(){
+    func resetingCalculator() {
         delegate?.viewEqualNone()
         text = ""
     }
-    func addingText(addText: String){
+    func addingText(addText: String) {
         text.append(addText)
     }
-    func addingOperand(operand: String){
+    func addingOperand(operand: String) {
         if canAddOperator {
             delegate?.insertToTextView(add: operand)
             addingText(addText: operand)
@@ -44,104 +44,87 @@ class Calculator {
             return
         }
     }
-    func addingNumberText(numbertext: String){
+    func addingNumberText(numbertext: String) {
         if expressionHaveResult {
             delegate?.insertToTextView(add: "")
         }
         delegate?.insertToTextView(add: numbertext)
         addingText(addText: numbertext)
     }
-    func checkIfThereIsAPriority() -> Bool {
-        if text.contains("/") || text.contains("x") {
-            return true
-        }
-        return false
+    
+    func printTheResult(of elements : [String]) {
+        delegate?.viewEqualNone()
+        delegate?.insertToTextView(add: elements[0])
+        text.removeAll()
+        addingText(addText: elements[0])
+    }
+    
+    func formatingNumbers(_ number: Double, style: NumberFormatter.Style = .decimal, maximumDigits: Int = 2) -> String? {
+        let formater = NumberFormatter()
+        formater.numberStyle = style
+        formater.maximumFractionDigits = maximumDigits
+        return formater.string(from: NSNumber(value: number))
     }
     
     func calculateThePriorityFirst() -> [String] {
         var priorityToReduce = elements
+        
         while priorityToReduce.contains(where: {$0 == "/" || $0 == "x"}) {
             if let index = priorityToReduce.firstIndex(where: {$0 == "/" || $0 == "x"}) {
                 guard let left = Double(priorityToReduce[index - 1]) else {break}
-                print(left)
                 let operand = priorityToReduce[index]
-                print(operand)
                 guard let right = Double(priorityToReduce[index + 1]) else {break}
-                print(right)
                 
                 var result : Double = 0
                 
                 switch operand {
                 case "x": result = left * right
-                case "/": result = left / right
+                case "/":
+                    if right == 0 {
+                        delegate?.alertFunction(title: "Error", message: "Can't divide by zero")
+                        resetingCalculator()
+                    } else {
+                        result = left / right
+                    }
                 default: delegate?.alertFunction(title: "Erreur", message: "Invalid input")
                 }
-                print(priorityToReduce, "Before the first index - 1")
-                priorityToReduce.remove(at: index - 1)
-                print(priorityToReduce, "After the first index - 1")
-                priorityToReduce.remove(at: index - 1)
-                print(priorityToReduce, "After the seconde index - 1")
-                priorityToReduce.remove(at: index - 1)
-                print(priorityToReduce, "After the third index - 1")
-                priorityToReduce.insert("\(result)", at: 2)
-                print(priorityToReduce, "Priority after insert result")
-                print(result, "Final result")
+                if let formateNumber = formatingNumbers(result){
+                    for _ in 0...2{
+                        priorityToReduce.remove(at: index - 1)
+                    }
+                    priorityToReduce.insert("\(formateNumber)", at: index - 1)
+                    printTheResult(of: priorityToReduce)
                 }
             }
-        print(priorityToReduce, "return priority")
-        return priorityToReduce
-        
         }
-    
-        
-    
-    
+        return priorityToReduce
+    }
     func calculate() {
         guard expressionIsCorrect && expressionHaveEnoughElement else {
             delegate?.alertFunction(title: "Error", message: "Invalid behavior. You must have at least 3 elements, and don't finish by an operator. ")
             return
         }
-        // Create local copy of operations
         var appendPriorityToElements = calculateThePriorityFirst()
-        var operationsToReduce = elements
-        print(operationsToReduce)
-       
+        while appendPriorityToElements.count > 1 {
             
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            
-            guard let left = Double(operationsToReduce[0]) else {return}
-            let operand = operationsToReduce[1]
-            guard let right = Double(operationsToReduce[2]) else {return}
+            guard let left = Double(appendPriorityToElements[0]) else {return}
+            let operand = appendPriorityToElements[1]
+            guard let right = Double(appendPriorityToElements[2]) else {return}
            
             var result: Double = 0
-            let formater = NumberFormatter()
-            formater.numberStyle = .decimal
-            formater.maximumFractionDigits = 2
             
             switch operand {
             case "+": result = left + right
             case "-": result = left - right
-            case "x": result = left * right
-            case "/":
-                if right == 0 {
-                    delegate?.alertFunction(title: "Error", message: "Can't divide by zero")
-                    resetingCalculator()
-                } else {
-                    result = left / right
-                }
-                
             default: delegate?.alertFunction(title: "Error", message: "Invalid operation")
             }
-            
-            if let formatedNumber = formater.string(from: NSNumber(value: result)) {
-                operationsToReduce = Array(operationsToReduce.dropFirst(3))
-                operationsToReduce.insert("\(formatedNumber)", at: 0)
-                delegate?.viewEqualNone()
-                delegate?.insertToTextView(add: operationsToReduce[0])
-                text.removeAll()
-                addingText(addText: operationsToReduce[0])
+            if let formatedNumber = formatingNumbers(result){
+                appendPriorityToElements = Array(appendPriorityToElements.dropFirst(3))
+                appendPriorityToElements.insert("\(formatedNumber)", at: 0)
+                printTheResult(of: appendPriorityToElements)
             }
+            
+            
         }
     }
 }
